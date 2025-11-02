8c88eeca69eda55963d65b7fa0c1e3aef13fd681
logonoff <git@logonoff.co>
2025-11-02 13:06:53 -0500

    dynamic scss banner

---
+++ workspaces/core/scripts/generate-banner.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview Generates the banner SCSS file with the current version.
+ */
+
+import { readFileSync, writeFileSync } from 'fs';
+
+const main = () => {
+    const { version } = JSON.parse(readFileSync('./package.json', 'utf-8'));
+    const banner = `
+/*!
+ * shale.css v${version} (MIT)
+ * https://github.com/logonoff/shalecss
+ */
+`.trimStart();
+
+    writeFileSync('./scss/_banner.scss', banner, 'utf-8');
+}
+
+main();
